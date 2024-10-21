9061fe5ec04d80b29d92fadfc378c95fe7a0a5e3
tunez1805 <salamiyusuf1998@gmail.com>
2024-10-21 05:31:55 -0700

    first commit

---
+++ src/app/component/useRef.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//import {useRef} from 'react'
+
+const useRef = require("react");
+
+export default function useRef(){
+    return (
+        <div>
+          
+        </div>
+      )
+}
+
+
+
+export default useRef
